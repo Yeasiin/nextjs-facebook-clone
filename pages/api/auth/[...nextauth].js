a803ed203775c29ac6dde3325b3db4b969f82591
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2021-09-08 00:14:45 +0600

    facebook auth fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,10 +7,11 @@ export default NextAuth({
     Providers.Facebook({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      singinUrl: process.env.NEXTAUTH_URL + "/api/auth/signin/facebook",
+      callbackUrl: process.env.NEXTAUTH_URL + "/api/auth/callback/facebook",
     }),
-
   ],
 
   // A database is optional, but required to persist accounts in a database
-//   database: process.env.DATABASE_URL,
-})
+  //   database: process.env.DATABASE_URL,
+});
